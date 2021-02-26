506c65f96a325dbaccc5f726e6beeb6a8409d233
MartinStudentProfile <576021@student.belgiumcampus.ac.za>
2021-02-26 12:37:08 +0200

    TestGit

---
+++ BusinessLogic_Layer/TestClass.java
@@ -0,0 +1,11 @@
+package BusinessLogic_Layer;
+/**
+ * TestClass
+ */
+public class TestClass
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Testing GIT");
+    }  
+}
