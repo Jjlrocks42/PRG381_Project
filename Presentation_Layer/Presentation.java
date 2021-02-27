2a96f04015104d337575c6a4330d1e68a31a6844
Jako <jakoswart40@gmail.com>
2021-02-27 16:11:58 +0200

    Chages 2

--- Presentation_Layer/Presentation.java
+++ Presentation_Layer/Presentation.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 import DataAccess_Layer.User_Bookings;
+import DataAccess_Layer.Items;
 
 public class Presentation 
 {
@@ -12,17 +13,30 @@ public class Presentation
     System.out.flush();  
         System.out.println("hi ");
     User_Bookings usr = new User_Bookings();
+    Items items= new Items();
     Scanner sc = new Scanner(System.in);
+
     System.out.println("Enter your name");
     String name = sc.next();
     usr.Getname(name); 
-
+    
     System.out.println("Enter booking your date");
     String d =sc.next();
+    
    // Date date = Date.parse(d);
    
     //usr.GetDate(date);
          usr.Text();
         
-      }
+
+
+        System.out.println("Enter item name");
+        String itemname = sc.next();
+        items.Getname(itemname);
+
+        items.Text();
+     }
+
+     
+
 }
