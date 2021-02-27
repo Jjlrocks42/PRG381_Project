58df4317107e450e9565bac0b5e5f81a0afa17bf
MartinStudentProfile <576021@student.belgiumcampus.ac.za>
2021-02-27 15:40:35 +0200

    datediff fuction done

--- BusinessLogic_Layer/DateDiff.java
+++ BusinessLogic_Layer/DateDiff.java
@@ -1,22 +1,25 @@
 package BusinessLogic_Layer;
+
 import java.util.Date;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.math.*;
+import java.text.SimpleDateFormat;
 
 
 public class DateDiff {
-    public void confirming(Date bookingDate, Float paid, float price){//confirming a booking
+    public void confirming(String bookingDate){//confirming a booking
         try{
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
 
-        String curDate = "01/02/2021";
-        String bookDate = "20/03/2021";
+        Date curDate = new Date();
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/uuuu");
+        String curdateString = formatDate.format(curDate);;
 
-        LocalDate date1 = LocalDate.parse(curDate,formatter);
-        LocalDate date2 = LocalDate.parse(bookDate,formatter);
+        LocalDate date1 = LocalDate.parse(curdateString,formatter);
+        LocalDate date2 = LocalDate.parse(bookingDate,formatter);
 
         long dateDiff = ChronoUnit.DAYS.between(date1, date2);
         
