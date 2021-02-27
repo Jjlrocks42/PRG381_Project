908c2150fb4912e082eec26096ab7e5ff506dd58
lourencowlov@gmail.com <lourencowlov@gmail.com>
2021-02-27 16:04:50 +0200

    pull`

--- BusinessLogic_Layer/Event.java
+++ BusinessLogic_Layer/Event.java
@@ -10,31 +10,7 @@ import java.util.Date;
 public class Event extends DataWriter
 {
 
-    public void confirming(Date bookingDate, Float paid, float price){//confirming a booking
-        try{
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/uuuu");
-
-        String curDate = "01/02/2021";
-        String bookDate = "20/03/2021";
-
-        LocalDate date1 = LocalDate.parse(curDate,formatter);
-        LocalDate date2 = LocalDate.parse(bookDate,formatter);
-
-        long dateDiff = ChronoUnit.DAYS.between(date1, date2);
-        
-        System.out.println(dateDiff); //unfinished
-           
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void dateAvailable(){
-        
-    }
-
-        
+   
 
     
     
