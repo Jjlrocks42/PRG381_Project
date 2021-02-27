7ef8864028691549ad2f29eb40bc6e575691658c
lourencowlov@gmail.com <lourencowlov@gmail.com>
2021-02-27 14:01:37 +0200

    dingus`

--- BusinessLogic_Layer/Event.java
+++ BusinessLogic_Layer/Event.java
@@ -2,26 +2,29 @@ package BusinessLogic_Layer;
 
 import java.util.Date;
 
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable.BinaryOp.And;
+import java.time.Duration;
 
 public class Event 
 {
 
-    public void confirming(Date bookingDate, Float paid, float price){//confirming a booking
+    public void confirming(Date bookingDate, float paid, float price){//confirming a booking
         try{
             Date dateCur = new Date();
             long days = Duration.between(dateCur, bookingDate).toDays();
-            FLoat p = paid/price;
+            float p = paid/price;
             if((!(days<15)) && (p>=0.5)){
                 //call data writer meth to confirm
             }
-        }catch(arseException e){
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
 
-    public void dateAvailable(){
-        
+    public boolean dateAvailable(Date bookingDate, Date nextClosestDate)
+    {
+      
+      return bookingDate.getTime() >= nextClosestDate.getTime();
+      
     }
 
 
